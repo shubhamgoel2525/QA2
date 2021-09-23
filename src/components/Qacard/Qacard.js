eb9370a975fe509667d1e820bb30421623ee9f28
Shubham Goel <shubhamgoel2525@gmail.com>
2021-09-23 14:20:10 +0530

    updated dependencies

--- src/components/Qacard/Qacard.js
+++ src/components/Qacard/Qacard.js
@@ -1,43 +1,57 @@
-import React, { Component } from 'react';
-import './Qacard.css';
-import downchevron from './down.svg';
-
+import React, { Component } from "react";
+import "./Qacard.css";
+import downchevron from "./down.svg";
 
 class Qacard extends Component {
-    state = {
-        showQuestionInfo: false
-    };
+  state = {
+    showQuestionInfo: false,
+  };
 
-    render() {
-        const { question, answer, company, author, difficulty } = this.props;
-        const { showQuestionInfo } = this.state;
-        // console.log(this.props);
-        return (
-
-            <div class="card-container">
-                <div className={`card ${difficulty}`}>
-                    <div class="Q-section">
-                        <div class="Q-container">
-                            <h4 class="Q-text">{question}</h4>
-                            <div class="detail-Container">
-                                <span class="sub-text">{company}</span>
-                                <span class="sub-text">Submitted By:<br /> {author}</span>
-                            </div>
-                        </div>
-                        <div className={`ControlBtn ${showQuestionInfo ? "rotate-180" : ""}`}><img onClick={() => this.setState({ showQuestionInfo: !this.state.showQuestionInfo})} src={downchevron} alt="down" style={{width:'3vw'}}></img></div>
-
-                    </div>
-
-                    {showQuestionInfo ? (
-                        <div class="A-section">
-                            <div class="A-container">
-                                <p class="Q-text">{answer}</p>
-                            </div>
-                        </div>) : null}
-                </div>
+  render() {
+    const { question, answer, company, author, difficulty } = this.props;
+    const { showQuestionInfo } = this.state;
+    
+    return (
+      <div className="card-container">
+        <div className={`card ${difficulty}`}>
+          <div className="Q-section">
+            <div className="Q-container">
+              <h4 className="Q-text">{question}</h4>
+              <div className="detail-Container">
+                <span className="sub-text">{company}</span>
+                <span className="sub-text">
+                  Submitted By:
+                  <br /> {author}
+                </span>
+              </div>
             </div>
-        )
-    }
+            <div
+              className={`ControlBtn ${showQuestionInfo ? "rotate-180" : ""}`}
+            >
+              <img
+                onClick={() =>
+                  this.setState({
+                    showQuestionInfo: !this.state.showQuestionInfo,
+                  })
+                }
+                src={downchevron}
+                alt="down"
+                style={{ width: "3vw" }}
+              ></img>
+            </div>
+          </div>
+
+          {showQuestionInfo ? (
+            <div className="A-section">
+              <div className="A-container">
+                <p className="Q-text">{answer}</p>
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Qacard;
--- src/components/Qacard/QuestionAnswer.js
+++ src/components/Qacard/QuestionAnswer.js
@@ -1,74 +1,74 @@
-import React, { Component } from 'react';
-import Qacard from './Qacard';
-import data from '../../assets/questions';
+import React, { Component } from "react";
+import Qacard from "./Qacard";
+import data from "../../assets/questions";
 
 class QuestionAnswer extends Component {
-    state = {
-        qa_data: [
-        {
-            // id: 1,
-            question: 'How does this Component Look?',
-            answer: 'And what about this answer?',
-            company: 'testcompany1',
-            author: 'JohnDoe1',
-            difficulty:"Easy"
-        },
-        {
-            // id: 2,
-            question: 'How does this 2nd Component Look?',
-            answer: 'And what about this answer?',
-            company: 'testcompany2',
-            author: 'JohnDoe2',
-            difficulty:"Medium"
-        },
-        {
-            // id: 3,
-            question: 'How does this 3rd Component Look?',
-            answer: 'And what about this answer?',
-            company: 'testcompany3',
-            author: 'JohnDoe3',
-            difficulty:"Hard"
-        }]
-    };
+  state = {
+    qa_data: [
+      {
+        // id: 1,
+        question: "How does this Component Look?",
+        answer: "And what about this answer?",
+        company: "testcompany1",
+        author: "JohnDoe1",
+        difficulty: "Easy",
+      },
+      {
+        // id: 2,
+        question: "How does this 2nd Component Look?",
+        answer: "And what about this answer?",
+        company: "testcompany2",
+        author: "JohnDoe2",
+        difficulty: "Medium",
+      },
+      {
+        // id: 3,
+        question: "How does this 3rd Component Look?",
+        answer: "And what about this answer?",
+        company: "testcompany3",
+        author: "JohnDoe3",
+        difficulty: "Hard",
+      },
+    ],
+  };
 
-    componentDidMount() {
-        data.forEach(ques => {
-            const obj = {
-                question: ques.question,
-                answer: ques.answer,
-                company: ques.company,
-                author: "",
-                difficulty: ques.level
-            }
+  componentDidMount() {
+    data.forEach((ques) => {
+      const obj = {
+        question: ques.question,
+        answer: ques.answer,
+        company: ques.company,
+        author: "",
+        difficulty: ques.level,
+      };
 
-            const arr = this.state.qa_data;
-            arr.push(obj);
+      const arr = this.state.qa_data;
+      arr.push(obj);
 
-            this.setState({
-                qa_data: arr
-            })
+      this.setState({
+        qa_data: arr,
+      });
+    });
+  }
 
-            console.log(this.state);
-        })
-    }
+  render() {
+    const { qa_data } = this.state;
 
-    render() {
-        const {qa_data} = this.state;
-
-        return(
-            <div>
-                {qa_data.map(qa => (
-                    <Qacard 
-                    question={qa.question}
-                    answer={qa.answer}
-                    company={qa.company}
-                    author={qa.author}
-                    difficulty={qa.difficulty}
-                    />
-                ))}
-            </div>
-        )
-    }
+    return (
+      <div>
+        {qa_data.map((qa, index) => (
+          <Qacard
+            key={index}
+            question={qa.question}
+            answer={qa.answer}
+            company={qa.company}
+            author={qa.author}
+            difficulty={qa.difficulty}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default QuestionAnswer;
